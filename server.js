3b9adad7dc657e95f0b39f87c33a1763763cf638
Navya Agarwal <82928853+navyagarwal@users.noreply.github.com>
2023-07-11 11:12:29 +0000

    Modules

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const bodyparser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
@@ -9,6 +11,17 @@ const PORT = process.env.PORT || 8080
 
 // log requests
 app.use(morgan('tiny'));
+
+// parse request to body-parser
+app.use(bodyparser.urlencoded({extended:true}))
+
+// set view enginer
+app.set("view engine", "ejs")
+
+// load assets
+app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
+app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
+app.use('/img', express.static(path.resolve(__dirname, "assets/img")))
 
 app.get('/', (req, res) =>{
     res.send("Password Manager Application");
